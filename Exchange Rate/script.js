94c39b1182d0e2f481b2d8f2596db6230a0a95df
Chris Rico <chris.r.rico@gmail.com>
2022-10-18 23:41:21 -0700

    added the fetch api, added key and api http request, added calculate function and data that is fetched

--- Exchange Rate/script.js
+++ Exchange Rate/script.js
@@ -8,10 +8,34 @@ const swap = document.getElementById('swap');
 
 // Fetch echange rate and update DOM
 
-
-
 function calculate() {
-    
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+
+    fetch(`https://v6.exchangerate-api.com/v6/8dda4296529ac95982cd73e9/latest/${currency_one}`)
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data)
+        const rate = data.conversion_rates[currency_two];
+
+        rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+        amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
 }
+
+// Event listeners
+
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+swap.addEventListener('click', () => {
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+    calculate();
+});
 
 calculate();
