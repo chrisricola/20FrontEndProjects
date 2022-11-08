9d12bd475cb47dbdf689a766534d3115bbbf11c6
Chris Rico <chris.r.rico@gmail.com>
2022-11-07 23:09:17 -0800

    added styling to the meal div, added random api and function and responsive

--- Meal Finder /index.js
+++ Meal Finder /index.js
@@ -55,6 +55,22 @@ function getMealByID(mealID) {
     });
 }
 
+// random meal from Api
+
+function getRandomMeal() {
+    // clear meals and heading
+    mealsEl.innerHTML = '';
+    resultHeading.innerHTML = '';
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+        .then(res => res.json())
+        .then(data => {
+            const meal = data.meals[0];
+
+            addMealToDom(meal);
+        });
+}
+
 //Meal to Dom
 function addMealToDom(meal) {
     const ingredients = [];
@@ -88,6 +104,7 @@ function addMealToDom(meal) {
 
 // Event Listeners
 submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
 
 mealsEl.addEventListener('click', e => {
     const mealInfo = e.path.find(item => {
