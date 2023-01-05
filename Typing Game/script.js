98432ce2bf172a99d7d6d7d4091ea5906f9f0517
Chris Rico <chris.r.rico@gmail.com>
2023-01-04 21:41:16 -0800

    added an update time function which updates the time on the DOM, clears after a word is typed in and adds more time, clears when the game is over and renders inner html elements for the score and button to restart game

--- Typing Game/script.js
+++ Typing Game/script.js
@@ -1,8 +1,8 @@
 const word = document.getElementById('word');
 const text = document.getElementById('text');
 const scoreEl = document.getElementById('score');
-const timeEl = document.getElementById('timeEl');
-const endgameEl = document.getElementById('endgameEl');
+const timeEl = document.getElementById('time');
+const endgameEl = document.getElementById('end-game-container');
 const settingsBtn = document.getElementById('settingsBtn');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settingsForm');
@@ -41,6 +41,12 @@ const words = [
   // Init time
   let time = 10;
 
+  // Focus text on start
+  text.focus();
+
+  // Start counting down
+  const timeInterval = setInterval(updateTime, 1000);
+
 
 // Generate random word from array
 function getRandomWord() {
@@ -59,6 +65,28 @@ function updateScore() {
     scoreEl.innerHTML = score;
   }
 
+// Update time
+function updateTime() {
+    time--;
+    timeEl.innerHTML = time + 's';
+
+    if(time === 0) {
+        clearInterval(timeInterval);
+        //endgame
+        gameOver();
+    }
+}
+
+function gameOver() {
+    endgameEl.innerHTML = `
+        <h1>Time ran out</h1>
+        <p>Your final score is ${score}</p>
+        <button onclick="location.reload()">Reload</button>
+    `;
+
+    endgameEl.style.display = 'flex';
+}
+
 addWordToDOM();
 
 
@@ -72,5 +100,9 @@ text.addEventListener('input', e => {
 
         // clear
         e.target.value = '';
+
+        time += 5;
+
+        updateTime();
     }
 }); 
