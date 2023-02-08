6a06b1c01bda0fd85c2e2e37d950c38c43a5ca7d
Chris Rico <chris.r.rico@gmail.com>
2023-02-07 22:06:35 -0800

    added a function to check the order of the richest people. If wrong then the name turns red, if correct the name turns green after pressing the button

--- Sortable List/script.js
+++ Sortable List/script.js
@@ -85,6 +85,20 @@ function swapItems(fromIndex, toIndex) {
     listItems[toIndex].appendChild(itemOne);
   }
 
+// check order of list items
+function checkOrder() {
+    listItems.forEach((listItem, index) => {
+        const personName = listItem.querySelector('.draggable').innerText.trim();
+
+        if(personName !== richestPeople[index]) {
+            listItem.classList.add('wrong');
+        } else {
+            listItem.classList.remove('wrong');
+            listItem.classList.add('right');
+        }
+    });
+}
+
 function addEventListeners() {
     const draggables = document.querySelectorAll('.draggable');
     const dragListItems = document.querySelectorAll('.draggable-list li');
@@ -99,3 +113,5 @@ function addEventListeners() {
         item.addEventListener('dragleave', dragLeave);
     });
 }
+
+check.addEventListener('click', checkOrder);
