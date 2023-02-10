d3062519041177344e25f01384b317243b99c520
Chris Rico <chris.r.rico@gmail.com>
2023-02-09 22:34:51 -0800

    added function that writes the message to see if its not a number, if the number is in range, to check number, and to see if the guess is higher or lower. Also added another event listener to the end of the game to play again

--- Speak Number Guessing/script.js
+++ Speak Number Guessing/script.js
@@ -15,8 +15,46 @@ recognition.start();
 function onSpeak(e) {
     const msg = e.results[0][0].transcript;
 
-    // writeMessage(msg);
-    // checkNumber(msg);
+    writeMessage(msg);
+    checkNumber(msg);
+}
+
+// Write what user speaks
+function writeMessage(msg) {
+    msgEL.innerHTML = `
+        <div>You Said: </div>
+        <span class="box">${msg}</span>
+    `;
+}
+
+// Check mesasge against number 
+function checkNumber(msg){
+    const num = +msg;
+
+    // Check if valid number
+    if(Number.isNaN(num)) {
+        msgEL.innerHTML += '<div> That is not a valid number</div>';
+        return;
+    }
+
+    // check if in range
+    if (num > 100 || num < 1) {
+        msgEL.innerHTML = '<div>Number must be between 1 and 100</div>';
+        return;
+    }
+
+    // Check number
+    if(num === randomNum) {
+        document.body.innerHTML = `
+            <h2>Congrats! You have guessed the number!<br><br>
+            It was ${num}</h2>
+            <button class="play-again" id="play-again">Play Again</button>
+        `;
+    } else if (num > randomNum) {
+        msgEL.innerHTML += '<div>GO LOWER</div>';
+    } else {
+        msgEL.innerHTML += '<div>GO HIGHER</div>';
+    }
 }
 
 function getRandomNumber() {
@@ -25,3 +63,12 @@ function getRandomNumber() {
 
 // Speak result
 recognition.addEventListener('result', onSpeak);
+
+// End SR service
+recognition.addEventListener('end', () => recognition.start());
+
+document.body.addEventListener('click', (e) => {
+    if(e.target.id == 'play-again') {
+        window.location.reload();
+    }
+});
